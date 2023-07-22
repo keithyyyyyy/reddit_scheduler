095e4bebf40505c6a78363816c11c6b1077d2a1c
Keith Tan GF <dirtypupkeith@live.com.sg>
2023-07-22 13:38:08 +0800

    requirements spell wrong

--- mongo_poll/poll_threads.py
+++ mongo_poll/poll_threads.py
@@ -10,9 +10,9 @@ def handler(event, context):
     # mongodb atlas connection
     uri = "mongodb+srv://" + os.getenv("MONGO_USERNAME") + ":" + os.getenv("MONGDO_PASSWORD") + "@reddit.hsttylb.mongodb.net/?retryWrites=true"
     client = MongoClient(uri, tlsCAFile=certifi.where())
-    db = client['reddit_data']
-    threadsCollection = db['threads']
-    # userCollection = db["users"]
+    threads_db = client['reddit_data']
+    threadsCollection = threads_db['threads']
+    usersCollection = client["users"]["accounts"]
 
     # lambda boto3
     aws_lambda = boto3.client(
@@ -23,21 +23,26 @@ def handler(event, context):
 
     documents = threadsCollection.find({"status": "pending"})
     for doc in documents:
-        # userCollection.find({})
-        output = {}
+        refresh_token = ""
+        user = usersCollection.find({"providerAccountId": doc["userId"]})
+        for u in user:
+            refresh_token = u["refresh_token"]
         
+        output = {}
         output["type"] = doc["type"]
         output["title"] = doc["title"]
-        output["refreshToken"] = doc["refreshToken"]
+        output["refreshToken"] = refresh_token
         output["body"] = doc["body"]
         output["sr"] = doc["sr"]
         output["status"] = doc["status"]
         output["userId"] = doc["userId"]
         
-        response = aws_lambda.invoke(
-            FunctionName='reddit_scheduler',
-            InvocationType='Event',
-            Payload=output
-        )
+        print(output)
+        print( "\n\n")
+        # response = aws_lambda.invoke(
+        #     FunctionName='reddit_scheduler',
+        #     InvocationType='Event',
+        #     Payload=output
+        # )
 
 print(handler("",""))
